98c235676ecac8bafc76af325061736b1004f5f8
Anderson Rodrigues <anderson.rodrigues@gruposolar.com.br>
2024-08-15 10:17:58 -0300

    Push

--- resources/js/Components/site/BrandGrid/index.tsx
+++ resources/js/Components/site/BrandGrid/index.tsx
@@ -11,7 +11,7 @@ const BrandGrid = ({ data }: BrandProps) => {
         <section className="flex justify-center items-center p-8 bg-gray-100">
             <div className="container mx-auto flex justify-center max-w-screen-xl">
                 <div className="grid grid-cols-2 gap-8 text-gray-500 lg:grid-cols-5 items-center">
-                    {data.map((post: any, idx: number) => (
+                    {data?.map((post: any, idx: number) => (
                         <BrandContent>
                             <img width={60} src={`storage/uploads/${post?.featured}`} />
                         </BrandContent>
