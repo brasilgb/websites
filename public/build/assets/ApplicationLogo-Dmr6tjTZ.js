4d37a467f898d420caa14b649e9a9a74c143a89d
Anderson Rodrigues <anderson.rodrigues@gruposolar.com.br>
2024-08-19 08:00:55 -0300

    Push

--- public/build/assets/ApplicationLogo-Dmr6tjTZ.js
+++
@@ -1,1 +0,0 @@
-import{q as n,j as i}from"./app-OML9pDKY.js";const a=({className:g})=>{var t,s;const{datasite:o}=n().props;return i.jsx("div",{className:g,children:i.jsx("img",{src:`/storage/images/${(t=o.config)!=null&&t.logo?(s=o.config)==null?void 0:s.logo:"default.png"}`,alt:"Logo"})})},r=a;export{r as A};
--- public/build/assets/dataSelect-D5PcI7CM.js
+++
@@ -1,1 +0,0 @@
-const a=[{value:1,label:"Administrador"},{value:2,label:"Usuário"}],l=[{value:1,label:"Ativo"},{value:2,label:"Inativo"}];export{a as r,l as s};
