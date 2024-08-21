929feb9abc1edd356376f9ecd4b0ca2038a32d8d
Anderson Rodrigues <anderson.rodrigues@gruposolar.com.br>
2024-08-21 12:52:30 -0300

    Push

---
+++ public/build/assets/dataSelect-BhnCYlhX.js
@@ -0,0 +1,1 @@
+const e=[{value:"admin",label:"Administrador"},{value:"user",label:"Usuário"},{value:"customer",label:"Customer"}],a=[{value:1,label:"Ativo"},{value:2,label:"Inativo"}];export{e as r,a as s};
--- public/build/assets/dataSelect-Dw0r5RPk.js
+++
@@ -1,1 +0,0 @@
-const a=[{value:"admin",label:"Administrador"},{value:"user",label:"Usuário"}],e=[{value:1,label:"Ativo"},{value:2,label:"Inativo"}];export{a as r,e as s};
