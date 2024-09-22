bea2ab829dfd52c1184e307c2784401b976757a2
Anderson Brasil <andersonbrasil72@gmail.com>
2024-09-22 16:17:48 -0300

    Push

--- public/build/assets/index-C6T75EIX.js
+++
@@ -1,1 +0,0 @@
-function e(r){if(r)return r.length<11?(r=r.replace(/\D/g,""),r=r.replace(/^(\d{2})(\d{4})(\d{4})/,"($1) $2-$3"),r):(r=r.replace(/\D/g,""),r=r.replace(/^(\d{2})(\d{5})(\d{4})/,"($1) $2-$3"),r)}function n(r){var d=r;return d=d.replace(/\D/g,""),d=d.replace(/(\d+)(\d{2})$/,"$1,$2"),d=d.replace(/(\d)(?=(\d{3})+(?!\d))/g,"$1."),d=d,r=d}export{n as a,e as m};
