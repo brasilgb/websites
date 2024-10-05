1594014b072b7e2292b2950e83f412f21634ee50
Anderson Brasil <andersonbrasil72@gmail.com>
2024-10-05 18:30:49 -0300

    Push

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -4,8 +4,8 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 export const apiweb = axios.create({
-    baseURL: "http://localhost:8000/api/",
-    // baseURL: "https://eplusteutonia.com.br/api/",
+    // baseURL: "http://localhost:8000/api/",
+    baseURL: "https://eplusteutonia.com.br/api/",
     headers: {
         "Content-Type": "application/json",
     },
